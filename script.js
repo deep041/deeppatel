673749bb7a271ad680229163aae29271bd3c73c8
deep041 <34814971+deep041@users.noreply.github.com>
2020-05-14 08:47:12 +0530

    script file, style file and images

---
+++ script.js
@@ -0,0 +1,5 @@
+var hero = window.document.getElementById("hero");
+            var tween = new TimelineLite();
+            tween
+            .fromTo(hero, 1, {width: '50%'}, {width: '100%'})
+            
