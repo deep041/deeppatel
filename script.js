2f003df155068b1adc9cfbcd74bc468d1b3d933d
deep041 <34814971+deep041@users.noreply.github.com>
2021-06-22 23:18:19 +0530

    Code formatted

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var hero = window.document.getElementById("hero");
-            var tween = new TimelineLite();
-            tween
-            .fromTo(hero, 1, {width: '50%'}, {width: '100%'})
-            
+var tween = new TimelineLite();
+tween
+    .fromTo(hero, 1, {
+        width: '50%'
+    }, {
+        width: '100%'
+    })
